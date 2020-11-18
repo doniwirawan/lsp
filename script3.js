cd44882acccec3ae1fa39c8b41c2165ab3df8d66
doniwirawan <doniwirawan166@gmail.com>
2020-11-18 22:09:18 +0800

    input data to json

--- script3.js
+++ script3.js
@@ -1,3 +1,5 @@
+
+const fs = require('fs');
 const buttonSegitiga = document.querySelector('#button-segitiga');
 
 const hasil = document.querySelector('#hasil');
@@ -9,6 +11,18 @@ function hitungLuasSegitiga(e) {
     const tinggi = document.querySelector('#tinggi');
     const angka = 0.5 * alas.value * tinggi.value;
     hasil.innerText = angka;
+    let inputValue = {
+        input1: alas,
+        input2: tinggi,
+        total: angka
+    }
+    let data = JSON.stringify(inputValue);
+
+    fs.writeFileSync('file.json', data, finished);
+
+    function finished(err) {
+        console.log('success');
+    }
 }
 
 
