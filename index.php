82e030343ed696659ddd0bf6a3f5da911ed2a76d
doniwirawan <doniwirawan166@gmail.com>
2020-11-18 21:25:18 +0800

    adding php version

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.css">
+    <link rel="stylesheet" href="node_modules\@fortawesome\fontawesome-free\css\all.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container box">
+        <div class="col-lg-12 ">
+        <h1 class="text-center">Perhitungan Bangun Datar</h1>
+        <div class="container menu">
+            <div class="col-lg-12">
+                <div class="row mt-5">
+                    <div class="card segitiga">
+                        <a href="segitiga.php">
+                            <img src="https://cdn3.iconfinder.com/data/icons/virtual-notebook/16/button_shape_triangle-512.png" alt="" class="triangle">
+                            <p class="text-center mt-4 text-white text-capitalize">program perhitungan luas segitiga</p>
+                        </a>
+                    </div>
+                    <div class="card lingkaran">
+                        <a href="lingkaran.php">
+                            <i class="far fa-circle fa-5x text-white"></i>
+                            <p class="text-center mt-4 text-white text-capitalize">program perhitungan luas lingkaran</p>
+                        </a>
+                    </div>
+                    <div class="card persegi">
+                        <a href="persegi.php">
+                            <i class="far fa-square fa-5x text-white"></i>
+                            <p class="text-center mt-4 text-white text-capitalize">program perhitungan luas persegi</p>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    
+
+
+
+    <script src="node_modules\jquery\dist\jquery.js"></script>
+    <script src="node_modules\@fortawesome\fontawesome-free\js\all.js"></script>
+    <script src="node_modules\bootstrap\dist\js\bootstrap.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ persegi.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.css">
+    <link rel="stylesheet" href="node_modules\@fortawesome\fontawesome-free\css\all.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+        // untuk menghilangkan error
+        error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+        error_reporting(E_ERROR);
+
+        $lebar = $hasil = "";
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $lebar = test_input($_POST['nama']);
+            
+        }
+
+        function test_input($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        $total = $lebar * $lebar;
+    ?>
+    <div class="container kotak">
+        <a href="index.php" class="main-menu"><i class="fas fa-arrow-left mr-2"></i>Main Menu</a>
+        <h1 class="text-center">
+            Program Hitung Luas Persegi
+        </h1>
+        <div class="hasil">
+            <h1>Hasil : </h1>
+            <h1 id="hasil"><?= $total; ?></h1>
+        </div>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="mt-4">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                
+                <span class="input-group-text" id="basic-addon1"><i class="fas fa-ruler-vertical mr-1"></i>Lebar</span>
+            </div>
+            
+            <input type="number" class="form-control" placeholder="Sisi" aria-label="Username"
+                aria-describedby="basic-addon1" required id="sisi" name="lebar">
+        </div>
+       
+        <input type="submit" class="btn btn-outline-primary" id="button-persegi" ></input>
+    </form>
+    </div>
+    
+
+
+
+
+    <script src="node_modules\jquery\dist\jquery.js"></script>
+    <script src="node_modules\@fortawesome\fontawesome-free\js\all.js"></script>
+    <script src="node_modules\bootstrap\dist\js\bootstrap.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
